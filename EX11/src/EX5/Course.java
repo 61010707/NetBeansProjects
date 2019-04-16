c530b41c1e0e94b4c812651cc4b02662be0046d9
DrZin <61010707@kmitl.ac.th>
2019-04-16 22:01:43 +0700

    excersise 11 => ex5 (complete)

--- EX11/src/EX5/Course.java
+++ EX11/src/EX5/Course.java
@@ -3,12 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/** ************************************
+ *               Course                *
+ *-------------------------------------*
+ * -courseName: String                 *
+ * -students: ArrayList<String> * -numberOfStudents: int * +Course(courseName:
+ * String) * +getCourseName(): String * +addStudent(student: String): void *
+ * +dropStudent(student: String): void * +getStudents(): String[] *
+ * +getNumberOfStudents(): int * ***********************************
+ */
 package EX5;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author DrZin
  */
 public class Course {
-    
+
+    private String courseName;
+    private ArrayList<String> students;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+        students = new ArrayList<String>();
+    }
+
+    public void addStudent(String student) {
+        students.add(student);
+    }
+
+    public String[] getStudents() {
+        String[] a = new String[students.size()];
+        return students.toArray(a);
+    }
+
+    public int getNumberOfStudent() {
+        return students.size();
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        students.remove(student);
+    }
 }
--- EX11/src/EX5/Main.java
+++ EX11/src/EX5/Main.java
@@ -3,6 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/** *******************************************************************************
+ * (The Course class) Rewrite the Course class in Listing 10.6. Use an ArrayList  *
+ * to replace an array to store students. Draw the new UML diagram for the class. *
+ * You should not change the original contract of the Course class (i.e., the     *
+ * definition of the constructors and methods should not be changed, but the      *
+ * private members may be changed.)                                               *
+ ******************************************************************************** */
 package EX5;
 
 /**
@@ -10,5 +17,27 @@ package EX5;
  * @author DrZin
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Course course1 = new Course("Data Structures");
+        Course course2 = new Course("Database Systems");
+
+        course1.addStudent("Peter Jones");
+        course1.addStudent("Kim Smith");
+        course1.addStudent("Anne kennedy");
+
+        course2.addStudent("Peter Jones");
+        course2.addStudent("Steve Smith");
+
+        System.out.println("Number of students in course1: "
+                + course1.getNumberOfStudent());
+        String[] students = course1.getStudents();
+        for (int i = 0; i < course1.getNumberOfStudent(); i++) {
+            System.out.print(students[i] + ", ");
+        }
+
+        System.out.println();
+        System.out.print("Number of students in course2: "
+                + course2.getNumberOfStudent());
+    }
 }
