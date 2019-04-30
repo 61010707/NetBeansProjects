867ee591388ca8f480f35f6beb25859bc5e4e265
DrZin <61010707@kmitl.ac.th>
2019-04-30 15:22:28 +0700

    add e-> action

--- HelloFX/src/Chepter16/TestEvent.java
+++ HelloFX/src/Chepter16/TestEvent.java
@@ -26,9 +26,9 @@ import javafx.scene.paint.Paint;
  * @author DrZin
  */
 public class TestEvent extends Application {
-    
+
     CirclePane circlePane = new CirclePane();
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane borderPane = new BorderPane();
@@ -42,60 +42,60 @@ public class TestEvent extends Application {
         Scene scene = new Scene(borderPane, 250, 250);
         stage.setScene(scene);
         stage.show();
-        btEnlarge.setOnAction(new EnlargeHandler());
-        btShrink.setOnAction(new ShrinkHandler());
-        
+        btEnlarge.setOnAction(e ->circlePane.enlarge());
+        btShrink.setOnAction(e->circlePane.shrink());
+
     }
-    
+
     class CirclePane extends StackPane {
-        
+
         private Circle circle = new Circle(20);
-        
+
         public CirclePane() {
             circle.setFill(Color.CYAN);
             circle.setStroke(Color.BLACK);
             getChildren().add(circle);
-            
+
         }
-        
+
         void enlarge() {
             circle.setRadius(circle.getRadius() + 2);
         }
-        
+
         void shrink() {
             circle.setRadius(circle.getRadius() - 2);
         }
-        
+
     }
-    
+
     class EnlargeHandler implements EventHandler<ActionEvent> {
-        
+
         public EnlargeHandler() {
         }
-        
+
         @Override
         public void handle(ActionEvent t) {
             //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             circlePane.enlarge();
         }
-        
+
     }
-    
+
     class ShrinkHandler implements EventHandler<ActionEvent> {
-        
+
         public ShrinkHandler() {
         }
-        
+
         @Override
         public void handle(ActionEvent t) {
             //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             circlePane.shrink();
         }
-        
+
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
